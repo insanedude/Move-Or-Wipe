f06a9715ecfbd8974d9ed7ceec91b842f3e2fae7
insanedude <insane.dude.btw@gmail.com>
2025-02-06 15:19:22 +0200

    added mobile movement support

--- Assets/Scripts/AddMovement.cs
+++ Assets/Scripts/AddMovement.cs
@@ -1,31 +1,33 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class AddMovement : MonoBehaviour
 {
     public Rigidbody rb;
     public float forwardForce = 100f;
 
+    public DefaultInputActions defaultInputActions;
     void FixedUpdate()
     {
-        if (Input.GetKey("w"))
-        {
-            Time.timeScale = 1;
-            rb.AddForce(0, 0, forwardForce * Time.deltaTime, ForceMode.VelocityChange);
-        }
-        if (Input.GetKey("s"))
-        {
-            Time.timeScale = 1;
-            rb.AddForce(0, 0, -forwardForce * Time.deltaTime, ForceMode.VelocityChange);
-        }
-        if (Input.GetKey("d"))
-        {
-            Time.timeScale = 1;
-            rb.AddForce(forwardForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
-        if (Input.GetKey("a"))
-        {
-            Time.timeScale = 1;
-            rb.AddForce(-forwardForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
+        Vector2 inputedButtons = defaultInputActions.Player.Move.ReadValue<Vector2>();
+        Vector3 movement = new Vector3(inputedButtons.x, 0, inputedButtons.y) * forwardForce * Time.deltaTime;
+        Time.timeScale = 1;
+        rb.AddForce(movement, ForceMode.VelocityChange);
+    }
+
+    private void Awake()
+    {
+        defaultInputActions = new DefaultInputActions();
+    }
+
+    private void OnEnable()
+    {
+        defaultInputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        defaultInputActions.Disable();
     }
 }
